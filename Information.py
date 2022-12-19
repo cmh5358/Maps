f53fd71f85361d4b04ed425713454d37c2bd64e4
cmh5358 <121001203+cmh5358@users.noreply.github.com>
2022-12-18 22:23:26 -0800

    Uploading Info doc for Maps

---
+++ Information.py
@@ -0,0 +1,24 @@
+'''
+Title: Publication Maps
+Author: Caitlin Hartig
+Date: 12/8/22
+
+This repository contains maps created using ArcGIS that were published in the following publications:
+
+   Porous Media of the Red River Formation, Williston Basin, North Dakota: 				                 September 2016
+        A Possible Sedimentary Enhanced Geothermal System                                                
+            International Journal of Earth Sciences                                                                
+               The final publication is available at Springer via http://dx.doi.org/10.1007/s00531-016-1398-9.
+               Completed research analysis of the shallow site conditions of sedimentary enhanced geothermal
+                   systems (SEGS) in the Red River Formation, ND, using ArcGIS.
+
+        GIS Tools Utilized: Ordinary Kriging, Co-Kriging
+
+   Discrete Fracture Network Simulation for Sedimentary Enhanced Geothermal Systems: 		             September 2015
+        Red River Formation, Williston Basin, ND                   
+           39th Geothermal Resources Council (GRC) Transactions Volume 39           	                            
+               Published at https://www.geothermal-library.org/index.php?mode=pubs&action=view&record=1032252.
+               Completed site response analysis and risk analysis of SEGS in the Red River Formation, ND, using MATLAB.
+
+        GIS Tools Utilized: Directional Distribution, Linear Directional Mean 
+'''
